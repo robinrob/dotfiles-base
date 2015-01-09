dbfd6d633a8b63969b77818ef6bb911873fe9824
Robin Smith <robin@mrrobinsmith.com>
2015-01-09 21:15:52 +0000

    Auto-update.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -860,6 +860,8 @@ alias dels="cd $SCREENSHOTS_HOME && rm Screen*"
 ###############################################################################
 # Functions
 ###############################################################################
+alias wk="wiki"
+
 alias ggt="google_translate"
 
 alias gg="google"
