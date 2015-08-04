b54ab4742bdf0537f43ec728808afb902cde4ad0
Linus Torvalds <torvalds@linux-foundation.org>
2015-08-05 00:55:16 +0100

    Quick-save.

---
+++ x.sh
@@ -0,0 +1,4 @@
+export GIT_AUTHOR_NAME="Linus Torvalds"
+export GIT_AUTHOR_EMAIL="torvalds@linux-foundation.org"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
