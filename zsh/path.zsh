045a53d0c55f480008f7b84cde938e3b137bc010
Robin Smith <robin@mrrobinsmith.com>
2015-11-02 15:28:57 +0000

    Quick-save.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -19,7 +19,7 @@ Dirs=(
   /usr/local/Library/anaconda3/bin
   ./node_modules/.bin
   /usr/local/Cellar/octave/4.0.0/bin
-  /usr/local/Cellar/macvim/7.4-77/bin
+  /usr/local/Cellar/macvim/7.4-79/bin
   /usr/local/Cellar/octave/4.0.0/bin
 )
 
