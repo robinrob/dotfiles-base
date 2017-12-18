45ca593fa0cbcb78111ee882b0f75be5a8c7f186
Robin Smith <robin@rsmith.io>
2017-12-18 13:32:38 +0000

    Quick-save.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -247,6 +247,8 @@ alias syslog="tail -f /var/log/system.log"
 ###############################################################################
 # Applications/tools
 ##############################################################################
+alias ev="open_app Evernote "
+
 alias xc="xcode"
 
 alias cle="open -a Clementine"
